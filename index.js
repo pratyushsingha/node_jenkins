c7ed45376aa98fe296a2cdc7c2f54ca83b453127
Pratyush Singha <pratyushsingha83@gmail.com>
2025-07-26 00:19:12 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Hello, test",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
